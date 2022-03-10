f6ef00e0ca700b7d59149ed5ce5df46cb3737740
Hodnex <Hodnex@gmail.com>
2022-03-10 11:33:40 +0300

    update

--- app/src/main/java/com/hodnex/todoapp/ToDoApplication.kt
+++ app/src/main/java/com/hodnex/todoapp/ToDoApplication.kt
@@ -1,9 +1,7 @@
-package com.hodnex.todoapp;
+package com.hodnex.todoapp
 
-import android.app.Application;
-
-import dagger.hilt.android.HiltAndroidApp;
+import dagger.hilt.android.HiltAndroidApp
+import android.app.Application
 
 @HiltAndroidApp
-public class ToDoApplication extends Application {
-}
+class ToDoApplication : Application()
